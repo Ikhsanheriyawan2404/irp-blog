68d3171ffb868ed9f87372333281caaab696f3da
ikhsanh <ikhsanheriyawan2404@gmail.com>
2021-02-23 23:18:10 +0700

    add routes

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
     <div class="container">
-        <a class="navbar-brand" href="index.html">My Blog</a>
+        <a class="navbar-brand" href="{{ route('home' )}}">My Blog</a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             Menu
             <i class="fas fa-bars"></i>
@@ -16,13 +16,13 @@
             </div>
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="about.html">Home</a>
+                    <a class="nav-link" href="{{ route('home') }}">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="contact.html">Tentang Kami</a>
+                    <a class="nav-link" href="{{ route('about_us') }}">Tentang Kami</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="contact.html">Galeri</a>
+                    <a class="nav-link" href="">Galeri</a>
                 </li>
                 @guest
                     @if (Route::has('login'))
@@ -42,8 +42,8 @@
                             {{ Auth::user()->name }}
                         </a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{-- {{ route('users.index', $user->id) }} --}}">My Profil (0)</a>
-                            <a class="dropdown-item" href="#">Admin</a>
+                            <a class="dropdown-item" href="{{ route('users.index', $user->id) }}">My Profil (0)</a>
+                            <a class="dropdown-item" href="{{ route('admin.index') }}">Admin</a>
                             <div class="dropdown-divider"></div>
                             <a class="dropdown-item" href="{{ route('logout') }}"
                                onclick="event.preventDefault();
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\{HomeController, UserController};
+use App\Http\Controllers\{HomeController, PostController, CategoryController, UserController};
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,19 +17,26 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/', HomeController::class)->name('home');
-Route::get('/{slug}', [HomeController::class, 'show'])->name('posts.index');
+Route::get('/{slug}', [HomeController::class, 'show'])->name('post.index');
 Route::get('/tentang-kami', [HomeController::class, 'about_us'])->name('about_us');
 Route::middleware('auth')->group(function () {
-    Route::prefix('users')->group(function() {
-        Route::get('{id}', [UserController::class, 'show'])->name('users.index');
-        Route::get('{slug}/edit', [PostController::class, 'edit'])->name('posts.edit');
-        Route::put('{slug}/edit', [PostController::class, 'update'])->name('posts.update');
+    Route::prefix('user')->group(function() {
+        Route::get('{id}', [UserController::class, 'show'])->name('user.index');
+        Route::get('{slug}/edit', [PostController::class, 'edit'])->name('user.edit');
+        Route::put('{slug}/edit', [PostController::class, 'update'])->name('user.update');
     });
-    Route::prefix('posts')->group(function () {
-        Route::get('create', [PostController::class, 'create'])->name('posts.create');
-        Route::post('store', [PostController::class, 'store'])->name('posts.store');
-        Route::get('{slug}/edit', [PostController::class, 'edit'])->name('posts.edit');
-        Route::put('{slug}/edit', [PostController::class, 'update'])->name('posts.update');
-        Route::delete('{slug}/delete', [PostController::class, 'destroy'])->name('posts.delete');
+    Route::prefix('post')->group(function () {
+        Route::get('create', [PostController::class, 'create'])->name('post.create');
+        Route::post('store', [PostController::class, 'store'])->name('post.store');
+        Route::get('{slug}/edit', [PostController::class, 'edit'])->name('post.edit');
+        Route::put('{slug}/edit', [PostController::class, 'update'])->name('post.update');
+        Route::delete('{slug}/delete', [PostController::class, 'destroy'])->name('post.delete');
+    });
+    Route::middleware('role:admin')->group(function () {
+        Route::prefix('admin')->group(function () {
+            Route::prefix('category')->group(function () {
+                Route::resources('category', CategoryController::class);
+            });
+        });
     });
 });
